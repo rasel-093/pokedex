9ea25f0d36e6e25f5d4df9bec53f44c96c0cd162
rasel-093 <rasel.cse19.ruet@gmail.com>
2025-07-27 18:58:51 +0600

    Favorite pokemon card implemented

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokedex/controllers/home_page_controller.dart';
 import 'package:pokedex/models/page_data.dart';
 import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/providers/pokemon_data_providers.dart';
+import 'package:pokedex/widgets/pokemon_card.dart';
 import 'package:pokedex/widgets/pokemon_list_tile.dart';
 
 final HomePageControllerProvider =
@@ -20,9 +22,9 @@ class _HomePageState extends ConsumerState<HomePage> {
   final ScrollController _allPokemonListScrollController = ScrollController();
   late HomePageController _homePageController;
   late HomePageData _homePageData;
+  late List<String> _favoritePokemons;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _allPokemonListScrollController.addListener(_scrollListener);
   }
@@ -46,6 +48,7 @@ class _HomePageState extends ConsumerState<HomePage> {
   Widget build(BuildContext context) {
     _homePageController = ref.watch(HomePageControllerProvider.notifier);
     _homePageData = ref.watch(HomePageControllerProvider);
+    _favoritePokemons = ref.watch(favoritePokemonsProvider);
     return Scaffold(body: _buildUi());
   }
 
@@ -60,9 +63,49 @@ class _HomePageState extends ConsumerState<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [_allPokeMons(context)],
+            children: [_favoritePokemonList(context), _allPokeMons(context)],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _favoritePokemonList(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Favorites", style: TextStyle(fontSize: 25)),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).width,
+            width: MediaQuery.sizeOf(context).height * 0.5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (_favoritePokemons.isEmpty)
+                  const Text("No favorite pokemons to show"),
+                if (_favoritePokemons.isNotEmpty)
+                  SizedBox(
+                    height: MediaQuery.sizeOf(context).height * 0.5,
+                    child: GridView.builder(
+                      scrollDirection: Axis.horizontal,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: _favoritePokemons.length,
+                      itemBuilder: (context, Index) {
+                        String pokemonUrl = _favoritePokemons[Index];
+                        return PokemonCard(pokemonUrl: pokemonUrl);
+                      },
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/pokemon_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/providers/pokemon_data_providers.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class PokemonCard extends ConsumerWidget {
+  final String pokemonUrl;
+  late FavoritePokemonsProvider _favoritePokemonsProvider;
+  PokemonCard({super.key, required this.pokemonUrl});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    _favoritePokemonsProvider = ref.watch(favoritePokemonsProvider.notifier);
+    final pokemon = ref.watch(pokemonDataProvider(pokemonUrl));
+    return pokemon.when(
+      data: (data) {
+        return _card(context, false, data);
+      },
+      error: (error, StackTrace) {
+        return Text("Error $error");
+      },
+      loading: () {
+        return _card(context, true, null);
+      },
+    );
+  }
+
+  Widget _card(BuildContext context, bool isLoading, Pokemon? pokemon) {
+    return Skeletonizer(
+      enabled: isLoading,
+      ignoreContainers: true,
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: MediaQuery.sizeOf(context).width * 0.03,
+          vertical: MediaQuery.sizeOf(context).height * 0.01,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.sizeOf(context).width * 0.03,
+          vertical: MediaQuery.sizeOf(context).height * 0.01,
+        ),
+        decoration: BoxDecoration(
+          boxShadow: const [
+            BoxShadow(color: Colors.black26, spreadRadius: 2, blurRadius: 10),
+          ],
+          borderRadius: BorderRadius.circular(15),
+          color: Theme.of(context).primaryColor,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  pokemon?.name?.toUpperCase() ?? "Pokemon",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "#${pokemon?.id?.toString()}",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: CircleAvatar(
+                backgroundImage:
+                    pokemon != null
+                        ? NetworkImage(pokemon.sprites!.frontDefault!)
+                        : null,
+                radius: MediaQuery.sizeOf(context).height * 0.05,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "${pokemon?.moves?.length} Moves",
+                  style: const TextStyle(color: Colors.white),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _favoritePokemonsProvider.removeFavoritePokeman(pokemonUrl);
+                  },
+                  child: const Icon(Icons.favorite, color: Colors.red),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
